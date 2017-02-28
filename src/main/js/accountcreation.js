7fe0359189adc8eb780675679f79f88f9621e505
m4di <mglew@uwo.ca>
2017-02-28 00:31:25 -0500

    Add files via upload
    accountcreation.js which creates a user account if the user name is not already taken and displays a message saying the account was created successfully, and if the user name has already been taken, displays a message that the account could not be created as it already exists.

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -1,4 +1,67 @@
+//Madi Glew's 2212 React Tutorial
+//run in chrome!
+
 import React from 'react';
+
+var AccountCreationComponent = React.createClass({
+
+    //initiallizing user account
+    getInitialState () {
+        return {
+            name : "",
+            message : "..."
+        }
+    },
+
+    //handling the account name
+    handleNameChange (e) {
+        // Prevent following the link.
+        e.preventDefault();
+        this.setState({ name : e.target.value , message : "..."});
+
+
+    },
+
+
+    //handling the message which displays whether or
+    //not the user accoutn has been created successfully
+    handleMessage(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.name;
+        fetch('http://localhost:8080/accountCreation/createAccount?'
+            + 'userName=' + name, {
+            method: 'POST'
+        }).then(res =>{
+            if(res.ok){
+                this.setState({name: this.state.name, message: 'Account Created!'});
+            }
+            else{
+
+                this.setState({ name: this.state.name, message: 'Account could not be created because the username is already taken.'});
+            }
+        })
+    },
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" defaultValue={this.state.name} onChange={this.handleNameChange} />
+                        <input type="submit" value="Create Account :)" onClick={this.handleMessage}/>
+                    </label>
+
+                </form>
+                Name: {this.state.name}
+                <br/>
+                Message: {this.state.message}
+                <br/>
+
+            </div>
+        );
+    }
+});
 
 export class AccountCreation extends React.Component {
 
@@ -9,7 +72,7 @@ export class AccountCreation extends React.Component {
     render() {
         return(
             <div>
-                Account creation elements go here.
+                <AccountCreationComponent/>
             </div>
         );
     }
